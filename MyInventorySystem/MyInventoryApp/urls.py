c78d7a7bc94585b0cab012d1dbf6c8ddab78567d
Pioo00 <piojose55@gmail.com>
2026-03-17 21:06:47 +0800

    started 2d

--- MyInventorySystem/MyInventoryApp/urls.py
+++ MyInventorySystem/MyInventoryApp/urls.py
@@ -21,4 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('view_supplier', views.view_supplier, name='view_supplier'),
+    path('add_bottle', views.add_waterbottle, name='add_bottle'),
 ]
